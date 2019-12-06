7f5d8b9d20ff65bb28432bf620e08a4891292a1c
Nadine Ju <nadine.jura@online.de>
2019-12-06 00:00:18 -0800

    mysql database connected

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ NPM PACKAGES
 *************************************/
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+/*************************************
+MYSQL
+*************************************/
+// link database
+const connection = mysql.createConnection({
+    // host, port, username, password, database
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bid_win_save_db"
+});
+
+connection.connect(function(err) {
+    console.log("Connected as ID: " + connection.threadId);
+});
